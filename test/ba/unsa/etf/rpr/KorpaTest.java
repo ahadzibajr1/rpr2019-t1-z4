1be64f9ae7e7f303981c2dc45662a361280028d1
Hadzibajramovic Amila <ahadzibajr1@etf.unsa.ba>
2020-11-02 12:40:05 +0100

    Dodan test dodajArtikl u testnu klasu KorpaTest
    Test provjerava da li će metoda dodajArtikl u klasi Korpa vratiti false ako pokušamo dodati više od 50 elemenata

--- test/ba/unsa/etf/rpr/KorpaTest.java
+++ test/ba/unsa/etf/rpr/KorpaTest.java
@@ -3,5 +3,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 
+
+
 public class KorpaTest {
+    @Test
+    public void dodajArtikl () {
+        Korpa k = new Korpa ();
+        for (int i=0; i<50; i++)
+            k.dodajArtikl(new Artikl("lopta", 10, "p"+i));
+
+        assertFalse (k.dodajArtikl (new Artikl("lopta", 10, "p" +100)));
+
+    }
 }
