1db578766e152019fdca484b7648d32e91093f20
Hadzibajramovic Amila <ahadzibajr1@etf.unsa.ba>
2020-11-02 13:02:38 +0100

    Dodan test dodajArtikl u testnu klasu SupermarketTest
    U testu se testira tvrdnja assertFalse koja provjerava da li će dodajArtikl (iz klase Supermarket) vratiti false vrijednost ako se pokuša dodati više od 1000 artikala u supermarket

--- test/ba/unsa/etf/rpr/SupermarketTest.java
+++ test/ba/unsa/etf/rpr/SupermarketTest.java
@@ -3,5 +3,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SupermarketTest {
+    @Test
+    public void dodajArtikl () {
+        Supermarket s = new Supermarket ();
+        for (int i=0; i<1000; i++)
+            s.dodajArtikl(new Artikl("lopta", 10, "p"+i));
 
+        assertFalse (s.dodajArtikl (new Artikl("lopta", 10, "p" +2000)));
+
+    }
 }
